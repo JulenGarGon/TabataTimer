ffd3e7de383a0df72135c0c1a6b6f21ec026cf54
Julen García Gonzalo <jgarciago.inf@upsa.es>
2025-05-03 15:09:17 +0200

    Edición inicial de resumen
    Cambio visual básico de resumen

--- TabataTimer/app/src/main/java/com/example/tabatatimer/resumen/Resumen.kt
+++ TabataTimer/app/src/main/java/com/example/tabatatimer/resumen/Resumen.kt
@@ -2,10 +2,14 @@ package com.example.tabatatimer.resumen
 
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.platform.LocalContext
@@ -34,8 +39,9 @@ fun Resumen(viewModel: ResumenViewModel = viewModel()){
 
     Box(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .background(Brush.horizontalGradient(listOf(Gris_Oscuro, Blanco, Gris_Claro)))
+            .padding(top = 10.dp)
     ) {
         LazyColumn {
             items(ejercicios.value) {
@@ -53,7 +59,27 @@ fun Resumen(viewModel: ResumenViewModel = viewModel()){
 
 @Composable
 fun EjercicioResumen(topEjercicio: TopEjercicio, onItemSelected: (TopEjercicio) -> Unit){
-    Text(text = "Ejercicio: ${topEjercicio.nombre}", color = Negro)
-    Text(text = "Repeticiones: ${topEjercicio.repeticiones}", color = Negro)
-    Text(text = "Peso: ${topEjercicio.peso}", color = Negro)
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(100.dp)
+        .padding(top = 5.dp, bottom = 5.dp, end = 5.dp, start = 5.dp)
+        .background(Negro)
+    ){
+        Column {
+            Text(text = "Ejercicio: ${topEjercicio.nombre}", color = Blanco)
+            Row (
+                horizontalArrangement = Arrangement.Center
+            ){
+                Text(text = "Repeticiones: ${topEjercicio.repeticiones}",
+                    color = Blanco,
+                    modifier = Modifier.weight(0.4f))
+                Spacer(modifier = Modifier.weight(0.2f))
+                Text(text = "Peso: ${topEjercicio.peso}",
+                    color = Blanco,
+                    modifier = Modifier.weight(0.4f))
+            }
+        }
+
+    }
+
 }
